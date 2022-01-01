0c145fe2b7b2503d92306591d6d9752e3a08a764
RPKennethPoh <83260144+RPKennethPoh@users.noreply.github.com>
2022-01-01 17:59:05 +0800

    Imported local files
    Added all the files that I've been working on locally.

---
+++ js/default.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $(".body-content").hide().toggleClass("hidden").delay(300).fadeIn(300);
+    $(".my-nav").click(function (e) {
+      /* 
+      need to store the current element being clicked here
+      because within setTimeout(), $(this) doesn't point to the element
+      but window because that's what is used to call the function setTimeout
+      */
+      var nav = $(this);
+      e.preventDefault();
+      $(".body-content").fadeOut(300);
+      setTimeout(function () {
+        if (nav.hasClass("nav-portfolio")) {
+          window.location.href = "portfolio.html";
+        } else if (nav.hasClass("nav-about")) {
+          window.location.href = "about.html";
+        } else if (nav.hasClass("nav-contact")) {
+          window.location.href = "contact.html";
+        } else if (nav.hasClass("nav-home")) {
+          window.location.href = "index.html";
+        }
+      }, 600);
+    });
+    var cursor = $(".cursor");
+
+    $(window).mousemove(function(e) {
+        cursor.css({
+            top: e.clientY - cursor.height() / 2,
+            left: e.clientX - cursor.width() / 2
+        });
+    });
+
+    $(window)
+        .mouseleave(function() {
+            cursor.css({
+                opacity: "0"
+            });
+        })
+        .mouseenter(function() {
+            cursor.css({
+                opacity: "1"
+            });
+        });
+
+    $("a")
+        .mouseenter(function() {
+            cursor.css({
+                transform: "scale(2)"
+            });
+        })
+        .mouseleave(function() {
+            cursor.css({
+                transform: "scale(1)"
+            });
+        });
+
+    $(window)
+        .mousedown(function() {
+            cursor.css({
+                transform: "scale(0.5)"
+            });
+        })
+        .mouseup(function() {
+            cursor.css({
+                transform: "scale(1)"
+            });
+        });
+  });
